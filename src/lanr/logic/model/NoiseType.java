00873d9c98280043d1f6fb240c0d610f44d7b970
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-18 02:32:21 +0100

    Added documentation.
    Added definitions.

--- src/lanr/logic/model/NoiseType.java
+++ src/lanr/logic/model/NoiseType.java
@@ -1,8 +1,40 @@
 package lanr.logic.model;
 
+/**
+ * @author Nicolas Bruch
+ * 
+ *         Type of noise found in an audio stream.
+ *
+ */
 public enum NoiseType {
-	Hum,
-	Clipping,
-	Silence,
-	Other;
+	/**
+	 * Occurrence of disturbing frequency.
+	 */
+	Hum("Occurrence of disturbing frequency"),
+	/**
+	 * Samples exceed the bit depth of the audio stream.
+	 */
+	Clipping("Samples exceed the bit depth of the audio stream"),
+	/**
+	 * Longer silent area in the audio stream.
+	 */
+	Silence("Longer silent area in the audio stream"),
+	/**
+	 * Difference in volume in the audio stream.
+	 */
+	Volume("Difference in volume in the audio stream"),
+	/**
+	 * Other noises.
+	 */
+	Other("Difference in volume in the audio stream");
+	
+	private final String definition;
+	
+	private NoiseType(String definition) {
+		this.definition = definition;
+	}
+	
+	public String getDefinition() {
+		return this.definition;
+	}
 }
