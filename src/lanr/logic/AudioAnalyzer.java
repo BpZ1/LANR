641402788964311f628c7b33c4bcf9577266f76c
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-18 02:14:39 +0100

    Changed background and stoke width.

--- src/lanr/logic/AudioAnalyzer.java
+++ src/lanr/logic/AudioAnalyzer.java
@@ -1,19 +1,32 @@
 package lanr.logic;
 
+
 import jwave.Transform;
-import jwave.transforms.wavelets.haar.Haar1;
+import jwave.transforms.wavelets.coiflet.Coiflet4;
+import ws.schild.jave.MultimediaInfo;
+import ws.schild.jave.MultimediaObject;
 import jwave.transforms.FastWaveletTransform;
 
 public class AudioAnalyzer {
 
 	public void anazlyze() {
-		Transform t = new Transform(new FastWaveletTransform(new Haar1()));
+		
+		
+		MultimediaInfo d;
+		MultimediaObject object = new MultimediaObject(null);
+		
+		Transform t = new Transform(new FastWaveletTransform(new Coiflet4()));
 		
 		double[ ] arrTime = { 1., 1., 1., 1., 1., 1., 1., 1. };
 
 		double[ ] arrFreq = t.forward( arrTime ); // 1-D DFT forward
 
+		for(int i = 0; i< arrFreq.length; i++) {
+			System.out.println(arrFreq[i]);			
+		}
+		
 		double[ ] arrReco = t.reverse( arrFreq ); // 1-D DFT reverse
+	
 		
 		for(int i = 0; i< arrReco.length; i++) {
 			System.out.println(arrReco[i]);			
