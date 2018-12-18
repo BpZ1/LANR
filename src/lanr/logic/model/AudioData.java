bfa237218a2804e6805cb1c2b1c901ff7861985d
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-18 02:32:07 +0100

    Added documentation.

--- src/lanr/logic/model/AudioData.java
+++ src/lanr/logic/model/AudioData.java
@@ -2,56 +2,61 @@ package lanr.logic.model;
 
 import java.util.List;
 
+/**
+ * @author Nicolas Bruch
+ * 
+ *         Represents the audio data from a given file.
+ * 
+ */
 public class AudioData {
 
 	private final String path;
+	/**
+	 * List of found {@link Noise} types in the different channel of this audio file.
+	 */
 	private List<Noise> foundNoise;
+	/**
+	 * Represents the amount and intensity of the found audio noises. 
+	 */
 	private double severity;
 	private final List<AudioChannel> audioChannels;
-		
+
 	public AudioData(String path, List<AudioChannel> audioChannels) {
 		this.audioChannels = audioChannels;
 		this.path = path;
-		System.out.println("Created data");
-		System.out.println("Channel: " + audioChannels.size());
-		for(AudioChannel c : audioChannels) {
-			System.out.println("Samples: " + c.getSamples().length);
-			System.out.println("SampleRate: " + c.getSampleRate());
-			System.out.println("BitRate: " + c.getBitDepth());
-		}
 	}
-	
+
 	private void calculateSeverity() {
 		severity = 0;
-		for(Noise noise : foundNoise) {
+		for (Noise noise : foundNoise) {
 			severity += noise.getSeverity();
 		}
 	}
-	
+
 	public int getSampleRate() {
-		if(audioChannels.size() > 0) {
+		if (audioChannels.size() > 0) {
 			return audioChannels.get(0).getSampleRate();
-		}else {
+		} else {
 			return 0;
 		}
 	}
-	
+
 	public int getBitDepth() {
-		if(audioChannels.size() > 0) {
+		if (audioChannels.size() > 0) {
 			return audioChannels.get(0).getBitDepth();
-		}else {
+		} else {
 			return 0;
 		}
 	}
-	
-	public List<AudioChannel> getAllChannel(){
+
+	public List<AudioChannel> getAllChannel() {
 		return audioChannels;
 	}
 
 	public AudioChannel getAudioChannel(int index) {
 		return audioChannels.get(index);
 	}
-	
+
 	public String getPath() {
 		return path;
 	}
@@ -63,17 +68,17 @@ public class AudioData {
 	public void setFoundNoise(List<Noise> foundNoise) {
 		this.foundNoise = foundNoise;
 	}
-	
+
 	public void addNoise(Noise noise) {
 		this.foundNoise.add(noise);
 		calculateSeverity();
 	}
-	
+
 	public void removeNoise(Noise noise) {
 		this.foundNoise.remove(noise);
 		calculateSeverity();
 	}
-	
+
 	public double getSeverity() {
 		return severity;
 	}
