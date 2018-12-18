6ab689b1cfbb4db165b3bdb5099f8a0a43c9ddcc
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-18 02:32:24 +0100

    Added documentation.

--- src/lanr/logic/model/AudioChannel.java
+++ src/lanr/logic/model/AudioChannel.java
@@ -1,24 +1,27 @@
 package lanr.logic.model;
 
+/**
+ * @author Nicolas Bruch
+ * 
+ *         Contains the data for a single audio channel.
+ *
+ */
 public class AudioChannel {
 
 	private int bitRate;
-	private int sampleRate; 
-	private double[] samples; 
-	
-	
+	private int sampleRate;
+	private double[] samples;
+
 	public AudioChannel(double[] samples, int bitRate, int sampleRate) {
 		this.samples = samples;
 		this.bitRate = bitRate;
 		this.sampleRate = sampleRate;
-		int duration = samples.length/sampleRate;
-		System.out.println("Length: " + duration + " seconds");
 	}
-	
+
 	public double[] getSamples() {
 		return samples;
 	}
-	
+
 	public int getBitDepth() {
 		return bitRate;
 	}
