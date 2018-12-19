8ad403d4159910e975fa97706a98b393f9c14445
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-19 01:11:22 +0100

    Added event handling.

--- src/lanr/model/MainModel.java
+++ src/lanr/model/MainModel.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lanr.logic.AudioAnalyzer;
+import lanr.logic.FileReader;
 import lanr.logic.model.AudioData;
 
 public class MainModel extends Model {
 
+	public static final String AUDIO_ADDED_PROPERTY = "addAudio";
+	public static final String AUDIO_REMOVED_PROPERTY = "removeAudio";
+	public static final String PROGRESS_UPDATE_PROPERTY = "prorgessUpdate";
+	
 	private List<AudioData> audioData = new ArrayList<AudioData>();
 	private AudioAnalyzer analyzer = new AudioAnalyzer();
 	
@@ -16,8 +21,14 @@ public class MainModel extends Model {
 		analyzer.anazlyze();
 	}
 	
-	public void addAudioData(AudioData data) {
+	/**
+	 * Adds an audio file to the model and fires the property change.
+	 * @param path - Path to the audio file
+	 */
+	public void addAudioData(String path) {	
+		AudioData data = FileReader.readFile(path);
 		audioData.add(data);
+		state.firePropertyChange(AUDIO_ADDED_PROPERTY, null, data);
 	}
 	
 	public List<AudioData> getAudioData(){
--- src/lanr/model/Model.java
+++ src/lanr/model/Model.java
@@ -1,5 +1,13 @@
 package lanr.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public abstract class Model {
 
+	protected final PropertyChangeSupport state = new PropertyChangeSupport(this);
+	
+	public void addChangeListener(PropertyChangeListener listener) {
+		this.state.addPropertyChangeListener(listener);
+	}
 }
