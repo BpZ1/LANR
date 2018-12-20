fdb95ab6929fbd0b96ef5e8ca14233eeda1cc805
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-20 01:24:05 +0100

    Added new Exception.

---
+++ src/lanr/logic/model/LANRException.java
@@ -0,0 +1,18 @@
+package lanr.logic.model;
+
+public class LANRException extends Exception {
+
+	private static final long serialVersionUID = -5064460493701095648L;
+
+	public LANRException(String message) {
+		super(message);
+	}
+	
+	public LANRException(String message, Throwable t) {
+		super(message, t);
+	}
+	
+	public LANRException(Throwable t) {
+		super(t);
+	}
+}
