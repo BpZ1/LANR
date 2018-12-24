609fc60ce921c2af1e9fcf55f054c8bfacb0bd0c
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-24 02:04:55 +0100

    Added main converter class.

---
+++ src/lanr/logic/utils/Converter.java
@@ -0,0 +1,41 @@
+package lanr.logic.utils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.humble.video.AudioFormat.Type;
+import lanr.logic.model.LANRException;
+
+public class Converter {
+
+	private static final Map<Type,DoubleConverter> converters;
+	static {
+		DoubleConverter b8Conv = new Byte8BitConverter();
+		DoubleConverter s16Conv = new Short16BitConverter();
+		DoubleConverter i32Conv = new Int32BitConverter();
+		DoubleConverter f32Conv = new Float32BitConverter();
+		DoubleConverter d64Conv = new Double64BitConverter();
+		
+        Map<Type,DoubleConverter> map = new HashMap<Type,DoubleConverter>();
+        map.put(Type.SAMPLE_FMT_DBL, d64Conv);
+        map.put(Type.SAMPLE_FMT_DBLP, d64Conv);
+        map.put(Type.SAMPLE_FMT_FLT, f32Conv);
+        map.put(Type.SAMPLE_FMT_FLTP, f32Conv);
+        map.put(Type.SAMPLE_FMT_S16, s16Conv);
+        map.put(Type.SAMPLE_FMT_S16P, s16Conv);
+        map.put(Type.SAMPLE_FMT_S32, i32Conv);
+        map.put(Type.SAMPLE_FMT_S32P, i32Conv);
+        map.put(Type.SAMPLE_FMT_U8, b8Conv);
+        map.put(Type.SAMPLE_FMT_U8P, b8Conv);
+        converters = Collections.unmodifiableMap(map);
+    }
+	
+	public static DoubleConverter getConverter(Type audioFormat) throws LANRException {
+		DoubleConverter converter = converters.get(audioFormat);
+		if(converter == null) {
+			throw new LANRException("Unsupported audio format: " + audioFormat.name());
+		}
+		return converter;
+	}
+}
