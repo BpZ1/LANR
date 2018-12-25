e723ca497dcc62e3b5507f3cf7e334d27dc00fd2
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-25 19:10:16 +0100

    Changed argument name.

--- src/lanr/view/AudioVisualisation.java
+++ src/lanr/view/AudioVisualisation.java
@@ -13,14 +13,14 @@ public class AudioVisualisation extends VBox {
 	
 	private static final String CSS_ID = "visualisationBackGround";
 
-	public AudioVisualisation(double height, double width, AudioData data) {	
+	public AudioVisualisation(double height, double minWidth, AudioData data) {	
 		this.setHeight(height);
 		this.setId(CSS_ID);	
 		double canvasHeight = height / data.getChannel().size();
 		//The canvas are created in advance and drawn to when analyzing
 		for (AudioChannel channel : data.getChannel()) {
 			this.getChildren().add(new ChannelVisualisation(
-					width,
+					minWidth,
 					canvasHeight,
 					channel));
 		}
