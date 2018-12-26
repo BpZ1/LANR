fbd0c3068ed5c7fb367641737fea813d28353bd5
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-26 04:44:59 +0100

    Changed window function.

--- src/lanr/logic/noise/WindowFunction.java
+++ src/lanr/logic/noise/WindowFunction.java
@@ -1,5 +1,7 @@
 package lanr.logic.noise;
 
+import lanr.logic.frequency.FrequencyConversion;
+
 public class WindowFunction {
 
 	private final double[] scalars;
@@ -7,25 +9,18 @@ public class WindowFunction {
 	private static final double PI = Math.PI;
 
 	public WindowFunction(int size, FrequencyConversion conversion) {
-	        scalars = new double[size];
-	        if(conversion == FrequencyConversion.DCT) {
-	        	for (int i = 0; i < size; i++) {
-		            
-		            double xx = Math.sin((PI/(2.0*size)) * (2.0 * i));
-		            scalars[i] = Math.sin((PI/2.0) * (xx * xx));
-		        }
-	        }else if(conversion == FrequencyConversion.FFT) {
-	        	for (int i = 0; i < size; i++) {
-	        		double xx = Math.sin((PI/(2.0*size)) * (i + 0.5));
-		            scalars[i] = Math.sin((PI/2.0) * (xx * xx));
-		        }
-	        }
-	        
-	    }
+		scalars = new double[size];
+		for (int i = 0; i < size; i++) {
+			double xx = Math.sin((PI / (2.0 * size)) * (2.0 * i));
+			scalars[i] = Math.sin((PI / 2.0) * (xx * xx));
+		}
+
+	}
 
 	public void apply(double[] data) {
 		if (data.length != scalars.length) {
-			throw new IllegalArgumentException("Invalid length. Was " + data.length + " but should be " + scalars.length);
+			throw new IllegalArgumentException(
+					"Invalid length. Was " + data.length + " but should be " + scalars.length);
 		}
 		for (int i = 0; i < data.length; i++) {
 			data[i] *= scalars[i];
