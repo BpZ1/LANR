12e0735854ecaecc2cb2cc14f04f1becffe1426b
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-26 04:42:29 +0100

    Added documentation.

--- src/lanr/logic/frequency/SampleConverter.java
+++ src/lanr/logic/frequency/SampleConverter.java
@@ -2,5 +2,15 @@ package lanr.logic.frequency;
 
 public interface SampleConverter {
 
+	/**
+	 * Converts the samples into frequency domain.
+	 * 
+	 * @param samples
+	 * @return Frequency magnitudes were <br>
+	 * i * fs / N <br>
+	 * N = Number of samples <br>
+	 * fs = sample frequency <br>
+	 * i = index of result
+	 */
 	public double[] convert(double[] samples);
 }
