7fac8006af32b233dc444d5832c936d335136b9a
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-31 02:53:11 +0100

    Added hamming window.

---
+++ src/lanr/logic/frequency/windowfunctions/HammingWindow.java
@@ -0,0 +1,32 @@
+package lanr.logic.frequency.windowfunctions;
+
+/**
+ * Implementation of the Hamming Window function.
+ * 
+ * @author Nicolas Bruch
+ *
+ */
+public class HammingWindow extends WindowFunction {
+
+	private final double[] scalars;
+
+	public HammingWindow(int size) {
+		scalars = new double[size];
+		for (int n = 0; n < size; n++) {
+			scalars[n] = 0.54 + 0.46 * Math.cos(((2.0 * PI) / (size - 1)) * n);
+		}
+
+	}
+	
+	@Override
+	public void apply(double[] samples) {
+		if (samples.length != scalars.length) {
+			throw new IllegalArgumentException(
+					"Invalid length. Was " + samples.length + " but should be " + scalars.length);
+		}
+		for (int i = 0; i < samples.length; i++) {
+			samples[i] *= scalars[i];
+		}	
+	}
+
+}
