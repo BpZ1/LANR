b1e7709565f83398d5116f669230624d1ec1fbe2
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-12-31 02:53:02 +0100

    Updated documentation.

--- src/lanr/logic/frequency/windowfunctions/VonHannWindow.java
+++ src/lanr/logic/frequency/windowfunctions/VonHannWindow.java
@@ -1,13 +1,20 @@
 package lanr.logic.frequency.windowfunctions;
 
+/**
+ * 
+ * Implementation of the Von-Hann Window function.
+ * 
+ * @author Nicolas Bruch
+ *
+ */
 public class VonHannWindow extends WindowFunction {	
 	
 	private final double[] scalars;
 	
 	public VonHannWindow(int windowSize) {
 		scalars = new double[windowSize];
-		for (int i = 0; i < windowSize; i++) {
-			scalars[i] = 0.5 * (1.0 - Math.cos((2.0 * PI * i) / (double)(windowSize - 1)));
+		for (int n = 0; n < windowSize; n++) {
+			scalars[n] = 0.5 * (1.0 - Math.cos((2.0 * PI * n) / (double)(windowSize - 1)));
 		}
 	}
 
