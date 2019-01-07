27d355b4c8747560f3a5305e9afdade52c38bb5b
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2019-01-07 02:43:02 +0100

    Changed output to range -1 and 1.

--- src/lanr/logic/utils/Byte8BitConverter.java
+++ src/lanr/logic/utils/Byte8BitConverter.java
@@ -4,19 +4,28 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 /**
- * @author Nicolas Bruch
+ * 
+ * Converter for 8 bit byte from byte array.
  *
- *         Converter for 8 bit byte from byte array.
+ * @author Nicolas Bruch
  *
  */
 public class Byte8BitConverter extends DoubleConverter {
 
+	private static final float MULTIPLIER = 1f / (float)Byte.MAX_VALUE;
+	
 	@Override
 	public double[] convert(byte[] data) {
 		double[] resultData = new double[data.length];
 		ByteBuffer buffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
 		for (int i = 0; i < resultData.length; i++) {
-			resultData[i] = buffer.get();
+			double value = ((float) buffer.get()) * MULTIPLIER;
+			if(value > 1) {
+				value = 1;
+			}else if(value < -1) {
+				value = -1;
+			}
+			resultData[i] = value;
 		}
 		return resultData;
 	}
