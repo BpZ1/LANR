0657ffb8c8e74d03706f4db67d1cada3caba0820
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2019-01-08 00:52:53 +0100

    Implemented silence search.

--- src/lanr/logic/noise/SilenceSearch.java
+++ src/lanr/logic/noise/SilenceSearch.java
@@ -1,30 +1,82 @@
 package lanr.logic.noise;
 
+import java.util.LinkedList;
 import java.util.List;
 
-import lanr.logic.model.AudioData;
 import lanr.logic.model.Noise;
+import lanr.logic.model.NoiseType;
 
 public class SilenceSearch extends NoiseSearch {
 
+	/**
+	 * Threshold of the audio signal under which it is
+	 * recognized as silence.
+	 */
+	private static double threshold = 0.02;
+	private static int maxSkip = 100;
+	/**
+	 * Number of seconds a silence has to last before it is
+	 * recognized as such.
+	 */
+	private static int minimalDuration = 5;
+		
+	private long sampleCounter = 0;
+	/**
+	 * Counts the silence samples
+	 */
+	private long counter = 0;
+	/**
+	 * Number of times a value over the threshold is ignored.
+	 */
+	private int skipCounter = 0;
+	
+	
+	private List<Noise> foundNoise = new LinkedList<Noise>();
+	private Noise currentNoise;
+	
 	public SilenceSearch(int sampleRate, int windowSize) {
 		super(sampleRate, windowSize);
 	}
-
+	
 	@Override
 	public void search(double[] samples) {
-		// TODO Auto-generated method stub
+		long minimalDurationSamples = minimalDuration * sampleRate;
+		for(double s : samples) {
+			sampleCounter++;
+			if(Math.abs(s) < threshold) {
+				counter++;
+				if(counter >= minimalDurationSamples) {
+					if(currentNoise != null) {
+						currentNoise.setLength(currentNoise.getLength() + 1);
+					}else {
+						currentNoise = new Noise(NoiseType.Silence, sampleCounter, minimalDurationSamples, 0);
+					}
+				}				
+			}else {
+				skipCounter++;
+				if(skipCounter > maxSkip) {
+					counter = 0;
+					skipCounter = 0;
+					if(currentNoise != null) {
+						foundNoise.add(currentNoise);
+						currentNoise = null;
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public List<Noise> getNoise() {
-		// TODO Auto-generated method stub
-		return null;
+		return foundNoise;
 	}
 
 	@Override
 	public void compact() {
-		// TODO Auto-generated method stub
+		if(currentNoise != null) {
+			foundNoise.add(currentNoise);
+		}
+		combineNoises(foundNoise, 1.0/sampleRate, sampleRate * 3);
 		
 	}
 
