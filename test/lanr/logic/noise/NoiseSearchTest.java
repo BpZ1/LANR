d0554bffa892c57eac4056868617a6e0374d0d4a
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2019-01-07 23:29:39 +0100

    Added tests for abstract noise search class.

---
+++ test/lanr/logic/noise/NoiseSearchTest.java
@@ -0,0 +1,68 @@
+package lanr.logic.noise;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import lanr.logic.model.Noise;
+
+/**
+ * Tests for the abstract noise search class.
+ * 
+ * @author Nicolas Bruch
+ *
+ */
+public class NoiseSearchTest {
+
+	/**
+	 * Noises created:<br>
+	 * Position - length<br>
+	 * 4 - 10<br>
+	 * 8 - 1<br>
+	 * 9 - 20<br>
+	 * 30 - 45<br>
+	 * 40 - 50
+	 */
+	private static StubNoiseSearch search;
+	/**
+	 * Number of noises found if distance was added
+	 */
+	private static final long DISTANCE_COUNT = 1;
+	/**
+	 * Number of noises found if no distance was added
+	 */
+	private static final long NO_DISTANCE_COUNT = 2;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		search = new StubNoiseSearch(1, 1);
+		search.search(null);
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void noExtraDistanceTest() {
+		List<Noise> noises = search.combineNoises(0);
+		assertEquals(NO_DISTANCE_COUNT, noises.size());
+		if (noises.size() == NO_DISTANCE_COUNT) {
+			assertEquals(25, noises.get(0).getLength());
+			assertEquals(60, noises.get(1).getLength());
+		}
+	}
+
+	@Test
+	void extraDistanceTest() {
+		List<Noise> noises = search.combineNoises(12);
+		assertEquals(DISTANCE_COUNT, noises.size());
+		if (noises.size() == DISTANCE_COUNT) {
+			assertEquals(86, noises.get(0).getLength());
+		}
+	}
+}
---
+++ test/lanr/logic/noise/StubNoiseSearch.java
@@ -0,0 +1,47 @@
+package lanr.logic.noise;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import lanr.logic.model.Noise;
+import lanr.logic.model.NoiseType;
+
+public class StubNoiseSearch extends NoiseSearch {
+	
+	private List<Noise> noiseList = new LinkedList<Noise>();
+	
+	public StubNoiseSearch(int sampleRate, int windowSize) {
+		super(sampleRate, windowSize);
+	}
+	
+	@Override
+	public void search(double[] samples) {
+		Noise n1 = new Noise(NoiseType.Clipping, 4, 10, 0);
+		Noise n2 = new Noise(NoiseType.Clipping, 8, 1, 0);
+		Noise n3 = new Noise(NoiseType.Clipping, 9, 20, 0);
+		Noise n4 = new Noise(NoiseType.Clipping, 30, 45, 0);
+		Noise n5 = new Noise(NoiseType.Clipping, 40, 50, 0);
+		
+		noiseList.add(n1);
+		noiseList.add(n2);
+		noiseList.add(n3);
+		noiseList.add(n4);
+		noiseList.add(n5);
+	}
+
+	@Override
+	public List<Noise> getNoise() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void compact() {
+		// TODO Auto-generated method stub
+	}
+	
+	public List<Noise> combineNoises(long distance) {
+		return combineNoises(noiseList, 0, distance);
+	}
+
+}
