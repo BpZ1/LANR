93fcdfe95200f900ed03d1894b33280008dd5021
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2019-01-08 00:53:24 +0100

    Implemented new clipping search algorithm.

--- src/lanr/logic/noise/ClippingSearch.java
+++ src/lanr/logic/noise/ClippingSearch.java
@@ -5,79 +5,111 @@ import java.util.List;
 
 import lanr.logic.model.Noise;
 import lanr.logic.model.NoiseType;
+import lanr.logic.model.Tuple;
+import lanr.model.Interval;
 
+/**
+ * 
+ * Implementation based on the algorithm proposed by
+ * Fanhu Bie, Dong Wang, Jun Wang, Thomas Fang Zheng.
+ * in their paper
+ * Detection and reconstruction of clipped speech for speaker recognition
+ * (see https://www.sciencedirect.com/science/article/abs/pii/S0167639315000722).
+ * 
+ */
 public class ClippingSearch extends NoiseSearch {	
 
 	public ClippingSearch(int sampleRate, int windowSize) {
 		super(sampleRate, windowSize);
 	}
 
+	private long locationCounter = 0;
+	
 	/**
-	 * Number of samples that have at the top of a square wave
+	 * Samples under this value will not be taken into account.
 	 */
-	private static final int THRESHOLD = 10;
-	private static final double SEVERITY = 0.2;
+	private static final double THRESHOLD = 0.02;
 	/**
-	 * Percentage of the amplitude that constitutes as clipping.
+	 * Number of sub intervals in which the samples are divided.
 	 */
-	private static final double BORDER_SIZE = 0.9;
+	private static final int INTERVALS = 20;
+	/**
+	 * If this percentage of samples is found in one interval
+	 * the samples are classified as clipped.
+	 */
+	private static double maxProportion = 0.40;
 	
 	private List<Noise> foundNoise = new LinkedList<Noise>();
-	private List<PotentialClipping> potentialNoise = new LinkedList<PotentialClipping>(); 
 	
-	private PotentialClipping currentNoise = null;
-	private long counter = 0;
-	
-	private double previousXCoordinate = 0.0;
-	private double previousYCoordinate = 0.0;
-	
-	private double maximum = Double.NEGATIVE_INFINITY;
-	private double minimum = Double.POSITIVE_INFINITY;
+	/**
+	 * List of intervals.
+	 */
+	private List<Tuple<Interval, Integer>> subIntervals;	
 	
 	@Override
 	public void search(double[] samples) {
-		
+		double maximum = getMaximumAmp(samples);
+		subIntervals = createSubIntervals(maximum);
+		//Sort all samples in their intervals
+		int samplesOverThreshold = 0;
 		for(double d : samples) {
-			this.maximum = Math.max(maximum, d);
-			this.minimum = Math.min(minimum, d);
-			
-			double slope = Math.abs(calculateSlope(previousYCoordinate, d));
-			
-			if(slope < 0.02 && slope > -0.02) {
-				counter++;
-				
-				if(counter >= THRESHOLD) {
-					if(currentNoise != null) {
-						currentNoise.addSample(d);
-					}else {
-						currentNoise = new PotentialClipping(new Noise(NoiseType.Clipping,
-								(int) previousXCoordinate + 1, THRESHOLD, 0));
-						
-						currentNoise.setMinAmp(d);
-						currentNoise.setMaxAmp(d);
-					}
-				}
-				//Set the previous point
-				previousYCoordinate = d;
-				previousXCoordinate++;
-			}else {
-				counter = 0;
-				if(currentNoise != null) {
-					this.potentialNoise.add(currentNoise);
-					currentNoise = null;
+			for(Tuple<Interval, Integer> subInterval : subIntervals) {
+				double value = Math.abs(d);
+				if(subInterval.x.contains(value)) {
+					subInterval.y = subInterval.y + 1;
+					samplesOverThreshold++;
 				}
 			}
-			
-			
 		}
-		//if distance is smaller than half of the average? or average count it
-		
-		//check the global maximum and minimum at the end and see which potential
-		//noises are within a certain range
+		double intervalValue = (double) subIntervals.get(INTERVALS - 1).y + (double) subIntervals.get(INTERVALS - 2).y;
+		double proportion = intervalValue / (double) samplesOverThreshold;
+		if(proportion > maxProportion) {
+			/*
+			 * Add noise if the current window has more than the above defined percentage of its
+			 * samples in the last interval.
+			 */
+			Noise noise = new Noise(NoiseType.Clipping, locationCounter, windowSize, 0);
+			foundNoise.add(noise);
+		}	
+		locationCounter += windowSize;
 	}
 	
-	private double calculateSlope(double y1, double y2) {
-		return y2 - y1; 
+	/**
+	 * Returns the maximum absolute value of the given samples.
+	 * @param samples - Samples for which the maximum is to be returned.
+	 * @return Maximum of the given samples.
+	 */
+	private double getMaximumAmp(double[] samples) {
+		double max = Double.NEGATIVE_INFINITY;
+		for(double d : samples) {
+			max = Math.max(Math.abs(d), max);		
+		}
+		return max;
+	}
+	
+	/**
+	 * Creates a list with the above defined number of intervals and an integer as counter.
+	 * The size of each interval is defined by the maximum amplitude given.
+	 * The first interval is smaller by the above defined threshold to filter out
+	 * white noise.
+	 * @param maximum - Maximum amplitude in the current window.
+	 * @return List of all intervals and their counter.
+	 */
+	private List<Tuple<Interval, Integer>> createSubIntervals(double maximum){
+		List<Tuple<Interval, Integer>> subIntervals = new LinkedList<Tuple<Interval, Integer>>();
+		double intervalSize = maximum / INTERVALS;	
+		double currentInterval = THRESHOLD;
+		for(int i = 1; i < INTERVALS; i++) {
+			double updatedInterval = i * intervalSize;
+			Interval interval = new Interval(currentInterval, updatedInterval);
+			currentInterval = updatedInterval;
+			subIntervals.add(new Tuple<Interval, Integer>(interval, 0));
+		}
+		//Adding last interval extra to avoid rounding errors
+		Interval interval = new Interval(currentInterval, maximum);
+		subIntervals.add(new Tuple<Interval, Integer>(interval, 0));
+		
+		return subIntervals;
 	}
 	
 	@Override
@@ -87,22 +119,7 @@ public class ClippingSearch extends NoiseSearch {
 
 	@Override
 	public void compact() {
-		if(currentNoise != null) {
-			this.potentialNoise.add(currentNoise);
-			currentNoise = null;
-		}
-		float topClippingBorder = (float) (BORDER_SIZE * maximum);
-		float bottomClippingBorder = (float) (BORDER_SIZE * minimum);
-		List<Noise> actualClipping = new LinkedList<Noise>();
-		for(PotentialClipping noise : potentialNoise) {
-			if(noise.getMaxAmp() >= topClippingBorder ||
-					noise.getMinAmp() <= bottomClippingBorder) {
-				System.out.println("Borders: " + bottomClippingBorder + " -> " + topClippingBorder);
-				System.out.println(noise.getMinAmp() + " -> " + noise.getMaxAmp());
-				actualClipping.add(noise.getNoise());
-			}
-		}
-		this.foundNoise = combineNoises(actualClipping, SEVERITY, sampleRate);
+		this.foundNoise = combineNoises(foundNoise, 1, sampleRate);
 	}
 	
 	
