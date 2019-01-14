d7e1ccc74fea22ec84029eb41f7db8d62fd668d4
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2019-01-14 03:53:07 +0100

    Added visualization of noise.

--- src/lanr/view/StreamVisualisation.java
+++ src/lanr/view/StreamVisualisation.java
@@ -2,11 +2,15 @@ package lanr.view;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.List;
 
 import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import lanr.logic.model.AudioStream;
+import lanr.logic.model.Noise;
+import lanr.logic.model.NoiseType;
 
 /**
  * Contains methods for the visual representation of an audio channel
@@ -14,13 +18,24 @@ import lanr.logic.model.AudioStream;
  * @author Nicolas Bruch
  * 
  */
-public class StreamVisualisation extends Canvas {	
+public class StreamVisualisation extends Canvas {
+	
 	private final double width;
 	private final AudioStream channel;
 	private final long sampleCount;
+	/**
+	 * Distance between samples.
+	 */
 	private final double sampleDistance;
-	private final double maxSampleValue;
+	/**
+	 * Height distance multiplier.
+	 */
 	private final double heightValue;
+	/**
+	 * Maximum sample size.<br>
+	 * Size goes from 100 to -100
+	 * or 0 to 200 in the canvas.
+	 */
 	private final double halfValue;
 	/**
 	 * Percentage of the frame that will be visualized.
@@ -35,9 +50,9 @@ public class StreamVisualisation extends Canvas {
 		this.getGraphicsContext2D().setStroke(Color.BLUE);
 		this.getGraphicsContext2D().setLineWidth(0.1);
 		channel.addChangeListener(createChangeListener());
-		this.sampleCount = (long) ((channel.getLength() * channel.getSampleRate()) 
-				* visualisationReductionFactor);
-
+		//Number of samples that will be displayed
+		this.sampleCount = (long) ((channel.getLength() * channel.getSampleRate()) * visualisationReductionFactor);
+		System.out.println(channel.getLength() + "   " + channel.getSampleRate() + "  " +  visualisationReductionFactor);
 		if (channel.getLength() / 3 < parentWidth) {
 			this.width = parentWidth;
 		} else {
@@ -45,41 +60,72 @@ public class StreamVisualisation extends Canvas {
 		}
 		this.setWidth(width);
 		this.sampleDistance = width / sampleCount;
-		this.maxSampleValue = 200;
+		double maxSampleValue = 200;
 		this.heightValue = height / maxSampleValue;
 		this.halfValue = maxSampleValue / 2;
 
 	}
 
 	private void drawAudioData(double[] data) {
-		Platform.runLater(()->{
-			// Take every Xth element
-			int sampleSize = (int) (data.length 
-					* visualisationReductionFactor);
-			int skipDistance = (int) (visualisationReductionFactor * 100);
-			double[] samples = new double[sampleSize];
-			int counter = 0;
-			for (int i = 0; i < samples.length; i++) {	
-				samples[i] = data[counter];		
-				counter += skipDistance;
-			}
+		// Take every Xth element
+		int sampleSize = (int) (data.length * visualisationReductionFactor);
+		int skipDistance = (int) (visualisationReductionFactor * 100);
+		double[] samples = new double[sampleSize];
+		int counter = 0;
+		for (int i = 0; i < samples.length; i++) {
+			samples[i] = data[counter];
+			counter += skipDistance;
+		}
 
-			double sampleYPosition;
-			for (double sample : samples) {
-				double newXPosition = currentXPosition + sampleDistance;
-				/*
-				 * Half width is added to convert from negative to positive. The height value
-				 * multiplier is needed to convert the sample value into the height space of the
-				 * canvas.
-				 */
-				sampleYPosition = (sample * 100 + halfValue) * heightValue;
-				this.getGraphicsContext2D().strokeLine(currentXPosition, lastYPosition, newXPosition, sampleYPosition);
-				// update last point
-				lastYPosition = sampleYPosition;
-				currentXPosition = newXPosition;
-			}
-			this.getGraphicsContext2D().fill();
-		});		
+		double sampleYPosition;
+		for (double sample : samples) {
+			double newXPosition = currentXPosition + sampleDistance;
+			/*
+			 * Half width is added to convert from negative to positive. The height value
+			 * multiplier is needed to convert the sample value into the height space of the
+			 * canvas.
+			 */
+			sampleYPosition = (sample * 100 + halfValue) * heightValue;
+			this.getGraphicsContext2D().strokeLine(currentXPosition, lastYPosition, newXPosition, sampleYPosition);
+			// update last point
+			lastYPosition = sampleYPosition;
+			currentXPosition = newXPosition;
+		}
+		this.getGraphicsContext2D().fill();
+	}
+	
+	private void drawNoiseIntervals(List<Noise> noises) {
+		GraphicsContext gc = this.getGraphicsContext2D();
+		gc.setGlobalAlpha(0.2);
+		double multiplier = visualisationReductionFactor * sampleDistance;
+		for(Noise noise : noises) {
+			gc.setFill(getNoiseColor(noise.getType()));
+			gc.fillRect(
+					noise.getLocation() * multiplier,
+					0,
+					noise.getLength() * multiplier,
+					this.getHeight());
+		}
+	}
+	
+	public static Color getNoiseColor(NoiseType type) {
+		switch(type) {
+			case Background:
+				return Color.YELLOW;
+				
+			case Clipping:
+				return Color.RED;
+				
+			case Hum:
+				return Color.GREEN;
+				
+			case Silence:
+				return Color.WHITE;
+				
+			case Volume:
+				return Color.PINK;
+		}
+		throw new IllegalArgumentException("Invalid noise type");
 	}
 
 	private PropertyChangeListener createChangeListener() {
@@ -87,17 +133,28 @@ public class StreamVisualisation extends Canvas {
 
 			@Override
 			public void propertyChange(PropertyChangeEvent evt) {
-				double[] dataPacket = (double[]) evt.getNewValue();
-				drawAudioData(dataPacket);
+				Platform.runLater(() -> {
+					switch (evt.getPropertyName()) {
+						case AudioStream.DATA_ADDED_PROPERTY:
+							double[] dataPacket = (double[]) evt.getNewValue();
+							drawAudioData(dataPacket);
+							break;
+						case AudioStream.ANALYZING_COMPLETE:
+							@SuppressWarnings("unchecked") 
+							List<Noise> noise = (List<Noise>) evt.getNewValue();
+							drawNoiseIntervals(noise);
+							break;
+					}
+				});
 			}
 		};
 		return listener;
 	}
-	
+
 	public static void setVisualisationReductionFactor(double value) {
 		visualisationReductionFactor = value;
 	}
-	
+
 	public static double getVisualisationReductionFactor() {
 		return visualisationReductionFactor;
 	}
