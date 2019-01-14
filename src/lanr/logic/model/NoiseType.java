a45286063a5da25b75e271d831d1bea3b934bfe3
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2019-01-14 04:44:42 +0100

    Changed descriptions.

--- src/lanr/logic/model/NoiseType.java
+++ src/lanr/logic/model/NoiseType.java
@@ -10,23 +10,24 @@ public enum NoiseType {
 	/**
 	 * Occurrence of disturbing frequency.
 	 */
-	Hum("Occurrence of disturbing frequency"),
+	Hum("Continuous low frequency noise (under 400 Hz)."),
 	/**
 	 * Samples exceed the bit depth of the audio stream.
 	 */
-	Clipping("Samples exceed the bit depth of the audio stream"),
+	Clipping("Samples exeeding the limit of their recording device."
+			+ " Causes distortion in the audio."),
 	/**
 	 * Longer silent area in the audio stream.
 	 */
-	Silence("Longer silent area in the audio stream"),
+	Silence("Longer silent area in the audio stream."),
 	/**
 	 * Difference in volume in the audio stream.
 	 */
-	Volume("Difference in volume in the audio stream"),
+	Volume("Very loud or quiet parts in the audio stream."),
 	/**
 	 * Background noise.
 	 */
-	Background("Difference in volume in the audio stream");
+	Background("Noises over a certain threshold outside the wanted frequency domain (outside 100 - 400 Hz).");
 	
 	private final String definition;
 	
