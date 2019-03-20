8dcacda4db6d6eb1a015ee6a1c26774c8491f67f
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2019-03-20 01:46:39 +0100

    Added parameter descriptions.

--- src/lanr/view/Descriptions.java
+++ src/lanr/view/Descriptions.java
@@ -13,4 +13,18 @@ public class Descriptions {
 	public static final String CONTRAST_DESCRIPTION = "Contrast of the drawn spectrogram.";
 	public static final String WINDOW_SIZE_DESCRIPTION = "Size of the window in which the file is scanned (in samples)."
 			+ " A bigger window also means more memory usage.";
+	
+	public static final String CLIPPING_THRESHOLD_DESCRIPTION = "Sensitivity threshold for the detection of clipping.";
+	public static final String VOLUME_THRESHOLD_DESCRIPTION = "Sensitivity threshold for the detection of volume.";
+	public static final String SILENCE_THRESHOLD_DESCRIPTION = "Sensitivity threshold for the detection of silence.";
+	public static final String HUMMING_THRESHOLD_DESCRIPTION = "Sensitivity threshold for the detection of humming.";
+	
+	public static final String HUMMING_WEIGHT_DESCRIPTION = "Weighting of found humming noise in the overall severity.";
+	public static final String SILENCE_WEIGHT_DESCRIPTION = "Weighting of found silence noise in the overall severity.";
+	public static final String VOLUME_WEIGHT_DESCRIPTION = "Weighting of found volume noise in the overall severity.";
+	public static final String CLIPPING_WEIGHT_DESCRIPTION = "Weighting of found clipping noise in the overall severity.";
+	
+	public static final String HUMMING_LENGTH_DESCRIPTION = "Length a humming noise has to have to be recognized.";
+	public static final String SILENCE_LENGTH_DESCRIPTION = "Length a silence noise has to have to be recognized.";
+	public static final String VOLUME_LENGTH_DESCRIPTION = "Length a volume noise has to have to be recognized.";
 }
